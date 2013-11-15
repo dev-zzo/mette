69b5b2f86af997244063bc1fac94cb88cc13e6b5
test user <gauri@tut.by>
2013-11-15 16:14:48 +0100

    Rewritten the emu code to use tables. Added a generator to build tables.

--- Makefile
+++ Makefile
@@ -7,15 +7,19 @@ export TARGET_ARCH
 
 # Build tools
 export CC:=gcc
+export HOST_CC:=gcc
 export LD:=gcc
 export AR:=ar
 export RANLIB:=ranlib
+export SIZE:=size
 
 # Build options
 CFLAGS:=-Os 
 export CFLAGS
 LDFLAGS:=-s
 export LDFLAGS
+
+export HOST_CFLAGS:=-O2 -std=c99
 
 .PHONY: all src clean
 
--- src/vm/vm_core.c
+++ src/vm/vm_core.c
@@ -1,5 +1,6 @@
 #include "vm_internal.h"
 #include "vm_opcodes.h"
+#include "vm_opcodes_traits.h"
 
 #define VM_ARCH_MLOAD32(a) (*(vm_operand_t *)(a))
 #define VM_ARCH_MSTORE32(a,x) *(vm_operand_t *)(a) = (x)
@@ -23,254 +24,213 @@ static uint32_t vm_fetch32(vm_context_t *ctx)
 	return v;
 }
 
+#include "vm_opcodes_traits.tab"
+
 /* Emulate one VM insn. */
 void vm_step(vm_context_t *ctx)
 {
+#include "vm_opcodes_switch.tab"
+	vm_operand_t scratchpad[4];
 	vm_opcode_t opcode = (vm_opcode_t)vm_fetch8(ctx);
-	
-	switch(opcode) {
-		case VMOP_ADD: {
-			vm_operand_t lhs, rhs, result;
-			lhs = vm_stack_pop(&ctx->dstack);
-			rhs = vm_stack_pop(&ctx->dstack);
-			result = lhs + rhs;
-			vm_stack_push(&ctx->dstack, result);
-			break;
-		}
-		
-		case VMOP_SUB: {
-			vm_operand_t lhs, rhs, result;
-			lhs = vm_stack_pop(&ctx->dstack);
-			rhs = vm_stack_pop(&ctx->dstack);
-			result = lhs - rhs;
-			vm_stack_push(&ctx->dstack, result);
-			break;
-		}
-		
-		case VMOP_AND: {
-			vm_operand_t lhs, rhs, result;
-			lhs = vm_stack_pop(&ctx->dstack);
-			rhs = vm_stack_pop(&ctx->dstack);
-			result = lhs & rhs;
-			vm_stack_push(&ctx->dstack, result);
-			break;
-		}
-		
-		case VMOP_OR: {
-			vm_operand_t lhs, rhs, result;
-			lhs = vm_stack_pop(&ctx->dstack);
-			rhs = vm_stack_pop(&ctx->dstack);
-			result = lhs | rhs;
-			vm_stack_push(&ctx->dstack, result);
-			break;
-		}
-		
-		case VMOP_XOR: {
-			vm_operand_t lhs, rhs, result;
-			lhs = vm_stack_pop(&ctx->dstack);
-			rhs = vm_stack_pop(&ctx->dstack);
-			result = lhs ^ rhs;
-			vm_stack_push(&ctx->dstack, result);
-			break;
-		}
-		
-		case VMOP_NOT: {
-			vm_operand_t lhs, result;
-			lhs = vm_stack_pop(&ctx->dstack);
-			result = ~lhs;
-			vm_stack_push(&ctx->dstack, result);
-			break;
-		}
-		
-		case VMOP_LSL: {
-			vm_uoperand_t lhs, rhs, result;
-			lhs = vm_stack_pop(&ctx->dstack);
-			rhs = vm_stack_pop(&ctx->dstack);
-			result = lhs << rhs;
-			vm_stack_push(&ctx->dstack, result);
-			break;
-		}
-		
-		case VMOP_LSR: {
-			vm_uoperand_t lhs, rhs, result;
-			lhs = vm_stack_pop(&ctx->dstack);
-			rhs = vm_stack_pop(&ctx->dstack);
-			result = lhs >> rhs;
-			vm_stack_push(&ctx->dstack, result);
-			break;
-		}
-		
-		case VMOP_ASR: {
-			vm_soperand_t lhs, rhs, result;
-			lhs = vm_stack_pop(&ctx->dstack);
-			rhs = vm_stack_pop(&ctx->dstack);
-			result = lhs >> rhs;
-			vm_stack_push(&ctx->dstack, result);
-			break;
-		}
-		
-		case VMOP_LD0: {
-			vm_stack_push(&ctx->dstack, 0);
-			break;
-		}
-		
-		case VMOP_LD1: {
-			vm_stack_push(&ctx->dstack, 1);
-			break;
-		}
-		
-		case VMOP_LDU8: {
-			vm_operand_t value = (uint8_t)vm_fetch8(ctx);
-			vm_stack_push(&ctx->dstack, value);
-			break;
-		}
-		
-		case VMOP_LDS8: {
-			vm_soperand_t value = (int8_t)vm_fetch8(ctx);
-			vm_stack_push(&ctx->dstack, (vm_operand_t)value);
-			break;
-		}
-		
-		case VMOP_LD32: {
-			vm_operand_t value = vm_fetch32(ctx);
-			vm_stack_push(&ctx->dstack, value);
-			break;
-		}
-		
-		case VMOP_LDMU8: {
-			vm_operand_t addr = vm_stack_pop(&ctx->dstack);
-			vm_operand_t value = *(uint8_t *)(addr);
-			vm_stack_push(&ctx->dstack, value);
-			break;
-		}
-		
-		case VMOP_LDMS8: {
-			vm_operand_t addr = vm_stack_pop(&ctx->dstack);
-			vm_operand_t value = *(int8_t *)(addr);
-			vm_stack_push(&ctx->dstack, value);
-			break;
-		}
-		
-		case VMOP_LDMU16: {
-			vm_operand_t addr = vm_stack_pop(&ctx->dstack);
-			vm_operand_t value = *(uint16_t *)(addr);
-			vm_stack_push(&ctx->dstack, value);
-			break;
-		}
-		
-		case VMOP_LDMS16: {
-			vm_operand_t addr = vm_stack_pop(&ctx->dstack);
-			vm_operand_t value = *(int16_t *)(addr);
-			vm_stack_push(&ctx->dstack, value);
-			break;
-		}
-		
-		case VMOP_LDM32: {
-			vm_operand_t addr = vm_stack_pop(&ctx->dstack);
-			vm_operand_t value = *(vm_operand_t *)(addr);
-			vm_stack_push(&ctx->dstack, value);
-			break;
-		}
-		
-		case VMOP_STMU8: {
-			vm_operand_t addr = vm_stack_pop(&ctx->dstack);
-			vm_operand_t value = vm_stack_pop(&ctx->dstack);
-			*(uint8_t *)addr = (uint8_t)value;
-			break;
-		}
-		
-		case VMOP_STMS8: {
-			vm_operand_t addr = vm_stack_pop(&ctx->dstack);
-			vm_operand_t value = vm_stack_pop(&ctx->dstack);
-			*(int8_t *)addr = (int8_t)value;
-			break;
-		}
-		
-		case VMOP_STMU16: {
-			vm_operand_t addr = vm_stack_pop(&ctx->dstack);
-			vm_operand_t value = vm_stack_pop(&ctx->dstack);
-			*(uint16_t *)addr = (uint16_t)value;
-			break;
-		}
-		
-		case VMOP_STMS16: {
-			vm_operand_t addr = vm_stack_pop(&ctx->dstack);
-			vm_operand_t value = vm_stack_pop(&ctx->dstack);
-			*(int16_t *)addr = (int16_t)value;
-			break;
-		}
-		
-		case VMOP_STM32: {
-			vm_operand_t addr = vm_stack_pop(&ctx->dstack);
-			vm_operand_t value = vm_stack_pop(&ctx->dstack);
-			*(vm_operand_t *)addr = value;
-			break;
-		}
-		
-		case VMOP_DUP: {
-			vm_operand_t value = vm_stack_pop(&ctx->dstack);
-			vm_stack_push(&ctx->dstack, value);
-			vm_stack_push(&ctx->dstack, value);
-			break;
-		}
-		
-		case VMOP_SWAP: {
-			vm_operand_t value1 = vm_stack_pop(&ctx->dstack);
-			vm_operand_t value2 = vm_stack_pop(&ctx->dstack);
-			vm_stack_push(&ctx->dstack, value1);
-			vm_stack_push(&ctx->dstack, value2);
-			break;
-		}
-		
-		case VMOP_POP: {
-			vm_stack_pop(&ctx->dstack);
-			break;
-		}
-		
-		case VMOP_BRS: {
-			int8_t offset = (int8_t)vm_fetch8(ctx);
-			ctx->pc += offset;
-			break;
-		}
-		
-		case VMOP_BRL: {
-			int32_t offset = (int32_t)vm_fetch32(ctx);
-			ctx->pc += offset;
-			break;
-		}
-		
-		case VMOP_BRC: {
-			int8_t offset = (int8_t)vm_fetch8(ctx);
-			vm_operand_t cond = vm_stack_pop(&ctx->dstack);
-			if (cond) {
-				ctx->pc += offset;
-			}
-			break;
-		}
-		
-		case VMOP_BRNC: {
-			int8_t offset = (int8_t)vm_fetch8(ctx);
-			vm_operand_t cond = vm_stack_pop(&ctx->dstack);
-			if (!cond) {
-				ctx->pc += offset;
-			}
-			break;
-		}
-		
-		case VMOP_CALL: {
-			int32_t offset = (int32_t)vm_fetch32(ctx);
-			vm_stack_push(&ctx->cstack, (vm_operand_t)ctx->pc);
-			ctx->pc += offset;
-			break;
-		}
-		
-		case VMOP_RET: {
-			void *target = (void *)vm_stack_pop(&ctx->cstack);
-			ctx->pc = target;
-		}
-		
-		default:
-			vm_panic("vm_step: unknown opcode.");
+	const vm_opcode_traits_t *traits = &vm_opcode_traits[opcode];
+	int sop_count, sop_index;
+
+	sop_count = traits->pop_count;
+	sop_index = 0;
+	while (sop_count) {
+		scratchpad[sop_index] = vm_stack_pop(&ctx->dstack);
+		sop_index++;
+		sop_count--;
 	}
+	
+	goto *(&&op_invalid + offtab[opcode]);
+	
+op_invalid:
+	vm_panic("vm_step: unknown opcode.");
+
+op_ADD:
+	scratchpad[0] = scratchpad[0] + scratchpad[1];
+	goto op_push;
+op_SUB:
+	scratchpad[0] = scratchpad[0] - scratchpad[1];
+	goto op_push;
+op_UMUL: {
+	uint64_t res = (uint64_t)scratchpad[0] * (uint64_t)scratchpad[1];
+	scratchpad[0] = res >> 32;
+	scratchpad[1] = res & 0xFFFFFFFFULL;
+	goto op_push;
+}
+op_SMUL: {
+	int64_t res = ((int64_t)scratchpad[0]) * ((int64_t)scratchpad[1]);
+	scratchpad[0] = res >> 32;
+	scratchpad[1] = (vm_operand_t)res;
+	goto op_push;
+}
+op_UDIV: {
+	vm_uoperand_t quot = (vm_uoperand_t)scratchpad[0] / (vm_uoperand_t)scratchpad[1];
+	vm_uoperand_t rem = (vm_uoperand_t)scratchpad[0] % (vm_uoperand_t)scratchpad[1];
+	scratchpad[0] = rem;
+	scratchpad[1] = quot;
+	goto op_push;
+}
+op_SDIV: {
+	vm_soperand_t quot = (vm_soperand_t)scratchpad[0] / (vm_soperand_t)scratchpad[1];
+	vm_soperand_t rem = (vm_soperand_t)scratchpad[0] % (vm_soperand_t)scratchpad[1];
+	scratchpad[0] = rem;
+	scratchpad[1] = quot;
+	goto op_push;
+}
+op_AND:
+	scratchpad[0] = scratchpad[0] & scratchpad[1];
+	goto op_push;
+op_OR:
+	scratchpad[0] = scratchpad[0] | scratchpad[1];
+	goto op_push;
+op_XOR:
+	scratchpad[0] = scratchpad[0] ^ scratchpad[1];
+	goto op_push;
+op_NOT:
+	scratchpad[0] = ~scratchpad[0];
+	goto op_push;
+op_LSL:
+	scratchpad[0] = scratchpad[0] << scratchpad[1];
+	goto op_push;
+op_LSR:
+	scratchpad[0] = ((vm_uoperand_t)scratchpad[0]) >> scratchpad[1];
+	goto op_push;
+op_ASR:
+	scratchpad[0] = ((vm_soperand_t)scratchpad[0]) >> scratchpad[1];
+op_CC_LT:
+	scratchpad[0] = ((vm_soperand_t)scratchpad[0]) < ((vm_soperand_t)scratchpad[1]);
+	goto op_push;
+op_CC_GT:
+	scratchpad[0] = ((vm_soperand_t)scratchpad[0]) > ((vm_soperand_t)scratchpad[1]);
+	goto op_push;
+op_CC_LE:
+	scratchpad[0] = ((vm_soperand_t)scratchpad[0]) <= ((vm_soperand_t)scratchpad[1]);
+	goto op_push;
+op_CC_GE:
+	scratchpad[0] = ((vm_soperand_t)scratchpad[0]) >= ((vm_soperand_t)scratchpad[1]);
+	goto op_push;
+op_CC_B:
+	scratchpad[0] = ((vm_uoperand_t)scratchpad[0]) < ((vm_uoperand_t)scratchpad[1]);
+	goto op_push;
+op_CC_A:
+	scratchpad[0] = ((vm_uoperand_t)scratchpad[0]) > ((vm_uoperand_t)scratchpad[1]);
+	goto op_push;
+op_CC_BE:
+	scratchpad[0] = ((vm_uoperand_t)scratchpad[0]) <= ((vm_uoperand_t)scratchpad[1]);
+	goto op_push;
+op_CC_AE:
+	scratchpad[0] = ((vm_uoperand_t)scratchpad[0]) >= ((vm_uoperand_t)scratchpad[1]);
+	goto op_push;
+op_CC_EQ:
+	scratchpad[0] = scratchpad[0] == scratchpad[1];
+	goto op_push;
+op_CC_NE:
+	scratchpad[0] = scratchpad[0] != scratchpad[1];
+	goto op_push;
+op_LDC0:
+	scratchpad[0] = 0;
+	goto op_push;
+op_LDC1: 
+	scratchpad[0] = 1;
+	goto op_push;
+op_LDC2: 
+	scratchpad[0] = 2;
+	goto op_push;
+op_LDCU8:
+	scratchpad[0] = (vm_uoperand_t)(uint8_t)vm_fetch8(ctx);
+	goto op_push;
+op_LDCS8:
+	scratchpad[0] = (vm_soperand_t)(int8_t)vm_fetch8(ctx);
+	goto op_push;
+op_LDC32:
+	scratchpad[0] = vm_fetch32(ctx);
+	goto op_push;
+op_LDMU8:
+	scratchpad[0] = (vm_uoperand_t)*(uint8_t *)(scratchpad[0]);
+	goto op_push;
+op_LDMS8:
+	scratchpad[0] = (vm_soperand_t)*(int8_t *)(scratchpad[0]);
+	goto op_push;
+op_LDMU16:
+	scratchpad[0] = (vm_uoperand_t)*(uint16_t *)(scratchpad[0]);
+	goto op_push;
+op_LDMS16:
+	scratchpad[0] = (vm_soperand_t)*(int16_t *)(scratchpad[0]);
+	goto op_push;
+op_LDM32:
+	scratchpad[0] = *(vm_operand_t *)(scratchpad[0]);
+	goto op_push;
+op_STU8:
+	*(uint8_t *)(scratchpad[0]) = (uint8_t)scratchpad[1];
+	goto op_push;
+op_STU16:
+	*(uint16_t *)(scratchpad[0]) = (uint16_t)scratchpad[1];
+	goto op_push;
+op_STM32:
+	*(vm_operand_t *)(scratchpad[0]) = scratchpad[1];
+	goto op_push;
+op_DUP:
+	scratchpad[1] = scratchpad[0];
+	goto op_push;
+op_SWAP: {
+	vm_operand_t temp;
+	temp = scratchpad[0];
+	scratchpad[0] = scratchpad[1];
+	scratchpad[1] = temp;
+	goto op_push;
+}
+op_POP:
+	goto op_push;
+op_BRS: {
+	int8_t offset = (int8_t)vm_fetch8(ctx);
+	ctx->pc += offset;
+	goto op_push;
+}
+op_BRL: {
+	int32_t offset = (int32_t)vm_fetch32(ctx);
+	ctx->pc += offset;
+	goto op_push;
+}
+op_BRT: {
+	int8_t offset = (int8_t)vm_fetch8(ctx);
+	if (scratchpad[0]) {
+		ctx->pc += offset;
+	}
+	goto op_push;
+}
+op_BRF: {
+	int8_t offset = (int8_t)vm_fetch8(ctx);
+	if (!scratchpad[0]) {
+		ctx->pc += offset;
+	}
+	goto op_push;
+}
+op_CALL: {
+	int32_t offset = (int32_t)vm_fetch32(ctx);
+	vm_stack_push(&ctx->cstack, (vm_operand_t)ctx->pc);
+	ctx->pc += offset;
+	goto op_push;
+}
+op_RET: {
+	void *target = (void *)vm_stack_pop(&ctx->cstack);
+	ctx->pc = target;
+	goto op_push;
+}
+op_NCALL: {
+	goto op_push;
+}
+	
+op_push:
+	sop_count = traits->push_count;
+	sop_index = 0;
+	while (sop_count) {
+		vm_stack_push(&ctx->dstack, scratchpad[sop_index]);
+		sop_index++;
+		sop_count--;
+	}
+	
 }
 
