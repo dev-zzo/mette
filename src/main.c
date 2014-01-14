3230e4fb5e6f9d33cfd7b0dcf9c7163746aca221
Administrator <user@example.com>
2014-01-14 17:22:14 +0100

    correct startup file. argc/argv available.

--- src/main.c
+++ src/main.c
@@ -6,12 +6,9 @@ const char string[] = "Hello world!\n\n"; // 14
 int main(int argc, char *argv[])
 {
 	//vm_load_exec(argv[1]);
-	return 0;
-}
-
-void __start(int argc, char *argv[])
-{
 	sys_write(1, string, 14);
 	sys_write(1, argv[0], 8);
+	sys_write(1, argv[1], 8);
 	sys_exit(24);
+	return 0;
 }
