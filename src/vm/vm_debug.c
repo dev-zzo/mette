31d8465fd3db61ba0a595952033a81d9da3fc2d6
Administrator <user@example.com>
2014-01-22 14:25:52 +0100

    vm_panic: now uses proper rtl_print.

--- src/vm/vm_debug.c
+++ src/vm/vm_debug.c
@@ -1,15 +1,14 @@
 #include "vm_internal.h"
-#include "xstring.h"
+#include "rtl_print.h"
 #include "syscalls.h"
-#include <stdlib.h>
-#include <stdarg.h>
 
 void __attribute__((noreturn)) vm_panic(const char *format, ...)
 {
 	va_list args;
 	va_start(args, format);
-	sys_write(2, format, xstrlen(format));
+	rtl_vprint_fd(2, format, args);
 	va_end(args);
 	sys_exit(1);
+	__builtin_unreachable();
 }
 
